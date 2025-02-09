49f3f9538dee28d94b97abe64bc63a720750b933
Haeun Lee <lhe6636@naver.com>
2025-02-09 23:15:27 +0900

    초기 커밋.

---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            "Hello World!".Print();
+        }
+    }
+}
